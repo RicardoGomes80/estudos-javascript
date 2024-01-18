e3f2a049c7b8c777c59290d13cd937318304cc87
Ricardo Gomes <ricardo.rgomes@live.com>
2024-01-18 15:46:04 -0300

    Laço de Repetição FOR

--- basico/introducao/lacos_repeticaoJS.js
+++ basico/introducao/lacos_repeticaoJS.js
@@ -4,3 +4,16 @@
 //     //--exewcutar o codigo
 // }
 
+// Exercicios
+
+let notas = [2,5,10,25,50,10,10,25,50,100,2,5,10,25,50,10,10,25,50,100,2,5,10,25,50,10,10,25,50,100,2,5,10,25,50,10,10,25,50,100,1148];
+let total = 0;
+
+for (let repeticoes = 0; repeticoes < notas.length; repeticoes ++){
+    //total = total + notas[repeticoes]
+    total += notas[repeticoes]
+
+}
+
+
+console.log('O total do seu dinheiro é R$',total)
