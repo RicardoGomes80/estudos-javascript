7dc88b7fc9ac2ba2aeac4455ba22eac95c26f2fc
Ricardo Gomes <ricardo.rgomes@live.com>
2024-01-23 11:57:10 -0300

    Objet Math e Numbers e Strings em Javascript

---
+++ basico/intermediario/aula_14/index.js
@@ -0,0 +1,17 @@
+//IEEE 754-2008 - Padrão de expressões numéricas dentro do Javascript
+
+let num1 = 0.7;  // Number
+let num2 = 0.1;  // Number
+
+
+//console.log(num1.toString() + num2); ele continua sendo um Number mas na hora de concatenar ele vira uma string
+
+//num1 = num1.toString(); // Aqui ele vira pra sempre uma string
+//console.log(num1.toString(2)) // Transforma em codigo binário
+//console.log(typeof num1);
+//console.log(num1.toFixed(2)); //Deixar ou diminuir as casas decimais
+/*console.log(Number.isInteger(num1)); // Ele confere se o numero é Inteiro ( Tipo da Variavel ) e traz True ou False
+console.log(Number.isInteger(num2)); */
+
+
+console.log(num1 + num2);
---
+++ basico/intermediario/aula_15/index.js
@@ -0,0 +1,18 @@
+// Objeto Math em Javascript - usado para calculos matemáticos
+
+//let num1 = 9.50;
+//let num2 = Math.floor(num1); // Arredondar o numero para baixo - numero Interiro 
+//let num2 = Math.ceil(num1); // Arrendonda pra cima o numero - NUmero inteiro ou flutuante
+//let num2 = Math.round(num1); // Arrendonda pra cuma o mais proximo da metad de numero flutuante
+
+/*
+    console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // Maior numero da sequencia 
+    console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // Menor numero da sequencia 
+    console.log(Math.random()); // Gera um numero aleatorio entre 0 e 1 se os parenteses estiverem vazios
+
+    const aleatorio = Math.round(Math.random() * (10 - 5) + 5); // Inserir outros metodos Math dentro do outro
+    console.log(aleatorio);
+*/
+
+
+//console.log(num2);
